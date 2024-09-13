eabed57448b7e407f1876fd83d365571b90a253f
Sergey Korolev <bongrichard@rambler.ru>
2024-09-14 00:54:34 +0300

    CRUD для продуктов и поставщиков (урлы, серилизаторы, представления)

--- network_objects/views.py
+++ network_objects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, generics
+from network_objects.models import Product, Supplier
+from network_objects.serializers import ProductSerializer, SupplierSerializer
 
-# Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class SupplierViewSet(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
