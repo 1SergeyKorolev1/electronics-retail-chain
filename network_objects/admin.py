c1929af8ca35b2535e6e7d0244e8b7af8ea37564
Sergey Korolev <bongrichard@rambler.ru>
2024-09-14 04:03:02 +0300

    update purveyor_link in admin panel

--- network_objects/admin.py
+++ network_objects/admin.py
@@ -16,8 +16,8 @@ class UserAdmin(admin.ModelAdmin):
 
     def purveyor_link(self, obj):
         if obj.purveyor:
-            return format_html('<a href="{0}">{1}</a>'.format(
-                reverse('admin:network_objects_supplier_change', args=(f'{obj.purveyor.pk}')), obj.purveyor))
+            return format_html('<a href="{0}">{1} {2}</a>'.format(
+                reverse('admin:network_objects_supplier_change', args=(f'{obj.purveyor.pk}')), obj.purveyor, obj.purveyor.pk))
         return obj.purveyor
 
     def clean_debt(self, request, queryset):
